4ff148030425a4955421b562105c8947e1f9c83a
macbook <hai.lemanh.1310@gmail.com>
2019-04-08 17:47:15 +0700

    update trang chủ 17h47 8/4/2019

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     </div>
     <div class="uk-grid-small" uk-grid>
         <div class="uk-width-expand@m">
-            <div class="uk-card card1 uk-grid-collapse uk-margin" uk-grid>
+            <div class="uk-card card1 uk-grid-collapse uk-margin-small" uk-grid>
                 <div class="uk-width-expand@s uk-card-media-left">
                     <div class="uk-cover-container">
                         <img src="https://znews-photo.zadn.vn/w660/Uploaded/lce_cjvcc/2019_04_08/121.jpg" alt="" uk-cover>
@@ -41,9 +41,99 @@
                     </div>
                 </div>
             </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner5.png" alt=""></a>
+            </div>
+            <div class="uk-child-width-1-2@m uk-grid-small" uk-grid>
+                <div>
+                    <div class="top_1 uk-flex uk-flex-middle uk-flex-between">
+                        <h3 class="title1 uk-text-uppercase">tin tức</h3>
+                    </div>
+                    <ul class="uk-list uk-list-divider list2">
+                        <?php
+                        $cars = array
+                        (
+                            'Nhà tuyển dụng cần gì ở sinh viên tốt nghiệp?',
+                            'Facebook khóa tài khoản của bà Phạm Thị Yến',
+                            'Hội thảo xây dựng báo cáo giữa kỳ dự án hợp tác Việt Nam - Liên minh Châu Âu về việc làm cho sinh viên',
+                            'Xét nghiệm HIV gã đàn ông đâm hàng loạt người dân Sài Gòn',
+                            'Đoàn xe vượt đèn đỏ ở Đà Nẵng của Tập đoàn Trung Nguyên',
+                        );
+
+                        foreach ($cars as $key => $value) { ?>
+                            <li>
+                                <a href="#"><?php echo $value; ?></a>
+                                <div>20/10/2017 - 2.399 lượt xem</div>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+                <div>
+                    <div class="top_1 uk-flex uk-flex-middle uk-flex-between">
+                        <h3 class="title1 uk-text-uppercase">Đào tạo và bồi dưỡng</h3>
+                    </div>
+                    <ul class="uk-list uk-list-divider list2">
+                        <?php
+                        $cars = array
+                        (
+                            'Nhà tuyển dụng cần gì ở sinh viên tốt nghiệp?',
+                            'Facebook khóa tài khoản của bà Phạm Thị Yến',
+                            'Hội thảo xây dựng báo cáo giữa kỳ dự án hợp tác Việt Nam - Liên minh Châu Âu về việc làm cho sinh viên',
+                            'Xét nghiệm HIV gã đàn ông đâm hàng loạt người dân Sài Gòn',
+                            'Đoàn xe vượt đèn đỏ ở Đà Nẵng của Tập đoàn Trung Nguyên',
+                        );
+
+                        foreach ($cars as $key => $value) { ?>
+                            <li>
+                                <a href="#"><?php echo $value; ?></a>
+                                <div>20/10/2017 - 2.399 lượt xem</div>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner6.png" alt=""></a>
+            </div>
+            <div class="top_1 uk-flex uk-flex-middle uk-flex-between uk-margin-small">
+                <h3 class="title1 uk-text-uppercase">tin tức - sự kiện</h3>
+                <a class="uk-margin-small-right" href="#">Xem thêm <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+            </div>
+            <div id="slider_news" class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
+
+                <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@m uk-child-width-1-4@l uk-grid-small" uk-grid>
+                    <?php for ($i=1; $i<=8; $i++) { ?>
+                    <li>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w660/Uploaded/lce_cjvcc/2019_04_08/121.jpg" alt="" uk-cover>
+                            <canvas width="195" height="132"></canvas>
+                        </div>
+                        <h3 class="title2"><a href="#">Tập huấn công tác hướng nghiệp, phân luồng và tư vấn học đường tại huyện Đồng Văn, tỉnh Hà Giang</a></h3>
+                    </li>
+                    <?php } ?>
+                </ul>
+
+                <a class="uk-position-center-left uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"><img src="imgs/ico_prev.png" alt=""></a>
+                <a class="uk-position-center-right uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"><img src="imgs/ico_next.png" alt=""></a>
+
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner7.png" alt=""></a>
+            </div>
         </div>
         <div class="uk-width-1-4@m">
-
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner1.png" alt=""></a>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner2.png" alt=""></a>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner3.png" alt=""></a>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner4.png" alt=""></a>
+            </div>
         </div>
     </div>
 </div>

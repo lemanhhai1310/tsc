edeb21cc9371d05c6e36060034ec306081ae215c
macbook <hai.lemanh.1310@gmail.com>
2019-04-08 11:52:52 +0700

    header

--- header.php
+++ header.php
@@ -40,6 +40,23 @@
      theme_color="#24892d">
 </div>
 <section id="root" class="uk-height-viewport uk-offcanvas-content">
-<div>
-    <div></div>
+<div class="uk-section-xsmall">
+    <div class="uk-container">
+        <div class="uk-grid-small uk-child-width-auto" uk-grid>
+            <div>
+                <div class="uk-grid-small" uk-grid>
+                    <div class="uk-width-auto">
+                        <figure><a href="#"><img src="imgs/logo.png" alt=""></a></figure>
+                    </div>
+                    <div class="uk-width-expand">
+                        <h2 class="uk-text-uppercase title_logo1">Bộ giáo dục và đào tạo</h2>
+                        <h3 class="uk-text-uppercase title_logo2">Trung tâm hỗ trợ đào tạo và cung ứng nhân lực</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
 </div>

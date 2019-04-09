e153520f851496c871365dbac0204a6e3f049644
macbook <hai.lemanh.1310@gmail.com>
2019-04-09 15:48:56 +0700

    update hỏi đáp chi tiết

--- hoidap-chitiet.php
+++ hoidap-chitiet.php
@@ -18,40 +18,108 @@
     </div>
     <div class="uk-grid-small" uk-grid>
         <div class="uk-width-expand@m">
-            <h3 class="title_c uk-text-uppercase uk-margin">hỏi đáp</h3>
-            <div>
-                <div class="hoidap1">
-                    <div class="uk-grid-small" uk-grid>
-                        <div class="uk-width-auto">
-                            <span class="hd_stt">01</span>
-                        </div>
-                        <div class="uk-width-expand">
-                            <h5 class="title_hd"><a href="hoidap-chitiet.php">Hội thảo xây dựng báo cáo giữa kỳ dự án hợp tác Việt Nam - Liên minh Châu Âu về việc làm cho sinh viên ?</a></h5>
-                            <p>Phó Giám đốc Trung tâm Hỗ trợ đào tạo và Cung ứng nhân lực, Bộ GD&ĐT). Cách mạng công nghiệp 4.0 đặt ra yêu cầu cao hơn cho người lao động với đòi hỏi hàng loạt kỹ năng như: sáng tạo, quản lý, trí tuệ cảm xúc, đàm phán, ....<a href="#">Xem chi tiết</a></p>
-                            <ul class="uk-grid-collapse uk-child-width-auto" uk-grid>
-                                <li>11.07.2016</li>
-                                <li>duynguyen@gmail.com</li>
-                            </ul>
-                        </div>
-                    </div>
+            <h3 class="title_thuvien1 uk-margin">Chất lượng đào tạo là thước đo trường đại học</h3>
+            <div class="uk-margin cauhoi">
+                <div class="question"><b>Hỏi:</b> Tập huấn công tác hướng nghiệp, phân luồng và tư vấn học đường tại huyện Đồng Văn, tỉnh Hà Giang?</div>
+                <div class="author">Bởi: lehunganh@gmail.com</div>
+                <ul class="uk-child-width-auto uk-grid-small" uk-grid>
+                    <li><i class="fa fa-calendar-check-o" aria-hidden="true"></i> Ngày đăng: 08:32 - 20/06/2015</li>
+                    <li><i class="fa fa-eye" aria-hidden="true"></i> Lượt xem: 1.688</li>
+                </ul>
+            </div>
+            <article>
+                <p>Trả lời: Trong những năm gần đây, công tác giáo dục hướng nghiệp, phân luồng tư và vấn học đường cho học sinh phổ thông được xã hội đặc biệt quan tâm, đã góp phần quan trọng vào việc hướng học, hướng nghiệp và phân luồng học sinh sau trung học cơ sở, trung học phổ thông nhằm đào tạo nguồn nhân lực đáp ứng nhu cầu xã hội.</p>
+                <p>Chỉ thị của Bộ trưởng Bộ Giáo dục và Đào tạo (GD&ĐT) về “Nhiệm vụ chủ yếu năm học 2017-2018” đã chỉ rõ: “Đổi mới nội dung, hình thức giáo dục hướng nghiệp gắn với thực tiễn sản xuất, kinh doanh tại địa phương; xây dựng cơ chế thu hút sự tham gia của cơ sở dạy nghề, doanh nghiệp trong xây dựng chương trình, tài liệu và đánh giá kết quả giáo dục hướng nghiệp ở trường phổ thông”; “Bố trí đủ số lượng và bồi dưỡng nâng cao chất lượng giáo viên làm công tác giáo dục hướng nghiệp trong các trường trung học phổ thông; từng bước nâng cao hiệu quả công tác giáo dục hướng nghiệp, giúp học sinh có nhận thức rõ về định hướng nghề nghiệp”.</p>
+                <p>Tập huấn bồi dưỡng cán bộ, giáo viên làm công tác hướng nghiệp, phân luồng và tư vấn học đường là một trong những hoạt động quan trọng của Trung tâm Hỗ trợ đào tạo và Cung ứng nhân lực, Bộ GD&ĐT (Trung tâm). Trong những năm qua, đội ngũ cán bộ, công chức, viên chức của Trung tâm cùng đội ngũ chuyên gia có nhiều kinh nghiệm từ các đơn vị của Bộ GD&ĐT, Bộ Lao động - Thương binh và Xã hội, một số bộ, ngành khác xây dựng chương trình, biên soạn tài liệu và thường xuyên phối, kết hợp với các đơn vị, các địa phương tổ chức các lớp tập huấn nhằm nâng cao trình độ chuyên môn, nghiệp vụ cho cán bộ giáo viên làm công tác hướng nghiệp, phân luồng và tư vấn học đường theo chủ trương chung của ngành và phù hợp với yêu cầu của từng địa phương.</p>
+                <p>Căn cứ quan điểm chỉ đạo của Lãnh đạo Bộ, thực hiện kế hoạch, nhiệm vụ công tác năm 2017, Trung  tâm Hỗ trợ Đào tạo và Cung ứng nhân lực, Bộ GD&ĐT phối hợp với UBND huyện và Phòng GD&ĐT Đồng Văn, tỉnh Hà Giang tổ chức đợt tập huấn cho giáo viên trên địa bàn huyện về Công tác hướng nghiệp, phân luồng và tư vấn học đường vào tháng 10/2017. Tham gia đợt tập huấn có 222 học viên là các cán bộ quản lý phòng GDĐT, cán bộ, giáo viên của các trường trung học cơ sở, phổ thông dân tộc bán trú tiểu học và trung học cơ sở, phổ thông dân tộc bán trú trung học cơ sở, phổ thông cơ sở trên địa bàn huyện Đồng Văn, tỉnh Hà Giang.</p>
+                <p class="uk-text-right"><b>Thạc sỹ, luật sư Lê Việt Nga</b></p>
+            </article>
+            <div class="tool_page uk-flex uk-flex-middle uk-flex-between">
+                <ul class="uk-child-width-auto uk-grid-small" uk-grid>
+                    <li><a href="#"><i class="fa fa-undo" aria-hidden="true"></i> Về trang trước</a></li>
+                    <li><a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i> Gửi email</a></li>
+                    <li><a href="#"><i class="fa fa-print" aria-hidden="true"></i> In trang</a></li>
+                </ul>
+                <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+            </div>
+            <h3 class="title_c uk-text-uppercase uk-margin">câu hỏi cùng thể loại</h3>
+            <ul class="uk-list uk-list-divider list2">
+                <?php
+                $cars = array
+                (
+                    'Nhà tuyển dụng cần gì ở sinh viên tốt nghiệp?',
+                    'Facebook khóa tài khoản của bà Phạm Thị Yến',
+                    'Hội thảo xây dựng báo cáo giữa kỳ dự án hợp tác Việt Nam - Liên minh Châu Âu về việc làm cho sinh viên',
+                    'Xét nghiệm HIV gã đàn ông đâm hàng loạt người dân Sài Gòn',
+                    'Đoàn xe vượt đèn đỏ ở Đà Nẵng của Tập đoàn Trung Nguyên',
+                );
+
+                foreach ($cars as $key => $value) { ?>
+                    <li>
+                        <a href="#"><?php echo $value; ?></a>
+                        <div>20:50 24/07/2016 - lequanghuu09@yahoo.com</div>
+                    </li>
+                <?php } ?>
+            </ul>
+            <div class="uk-card guicauhoi">
+                <div class="uk-card-header uk-padding-small">
+                    Gửi câu hỏi về TRUNG TÂM HỖ TRỢ ĐÀO TẠO VÀ CUNG ỨNG NHÂN LỰC, BỘ GIÁO DỤC VÀ ĐÀO TẠO
                 </div>
-                <div class="hoidap1">
-                    <div class="uk-grid-small" uk-grid>
-                        <div class="uk-width-auto">
-                            <span class="hd_stt">02</span>
-                        </div>
-                        <div class="uk-width-expand">
-                            <h5 class="title_hd"><a href="hoidap-chitiet.php">Hội thảo xây dựng báo cáo giữa kỳ dự án hợp tác Việt Nam - Liên minh Châu Âu về việc làm cho sinh viên ?</a></h5>
-                            <p>Phó Giám đốc Trung tâm Hỗ trợ đào tạo và Cung ứng nhân lực, Bộ GD&ĐT). Cách mạng công nghiệp 4.0 đặt ra yêu cầu cao hơn cho người lao động với đòi hỏi hàng loạt kỹ năng như: sáng tạo, quản lý, trí tuệ cảm xúc, đàm phán, ....<a href="#">Xem chi tiết</a></p>
-                            <ul class="uk-grid-collapse uk-child-width-auto" uk-grid>
-                                <li>11.07.2016</li>
-                                <li>duynguyen@gmail.com</li>
-                            </ul>
-                        </div>
-                    </div>
+                <div class="uk-card-body uk-padding-small">
+                    <form>
+                        <fieldset class="uk-fieldset">
+
+                            <legend class="uk-legend uk-margin">Trung tâm hỗ trợ đao tạo sẵn sàng giải đáp - trợ giúp khách hàng, đối tác trong mọi tình huống. Quý vị vui lòng điền thông tin theo biểu mẫu gửi cho chúng tôi.</legend>
+
+                            <div class="uk-grid-small" uk-grid>
+                                <div class="uk-width-1-1">
+                                    <input class="uk-input" type="text" placeholder="Tiều đề câu hỏi">
+                                </div>
+                                <div class="uk-width-1-2@s">
+                                    <select class="uk-select">
+                                        <option>Lĩnh vực câu hỏi</option>
+                                        <option>Option 02</option>
+                                    </select>
+                                </div>
+                                <div class="uk-width-1-2@s">
+                                    <select class="uk-select">
+                                        <option>Hỏi đơn vị</option>
+                                        <option>Option 02</option>
+                                    </select>
+                                </div>
+                                <div class="uk-width-1-3@s">
+                                    <input class="uk-input" type="text" placeholder="Họ tên">
+                                </div>
+                                <div class="uk-width-1-3@s">
+                                    <input class="uk-input" type="text" placeholder="CMND/Hộ chiếu">
+                                </div>
+                                <div class="uk-width-1-3@s">
+                                    <input class="uk-input" type="text" placeholder="Địa chỉ">
+                                </div>
+                                <div class="uk-width-1-2@s">
+                                    <input class="uk-input" type="text" placeholder="Điện thoại">
+                                </div>
+                                <div class="uk-width-1-2@s">
+                                    <input class="uk-input" type="text" placeholder="Email">
+                                </div>
+                                <div class="uk-width-1-1">
+                                    <textarea class="uk-textarea" rows="7" placeholder="Nội dung câu hỏi"></textarea>
+                                </div>
+                                <div class="uk-width-auto">
+                                    <input class="uk-input" type="text" placeholder="Mã bảo mật">
+                                </div>
+                                <div class="uk-width-auto">
+                                    <button type="submit" class="uk-button uk-button-danger">Gửi</button>
+                                </div>
+                                <div class="uk-width-auto">
+                                    <button type="button" class="uk-button uk-button-default">Hủy</button>
+                                </div>
+                            </div>
+
+                        </fieldset>
+                    </form>
                 </div>
             </div>
-            <?php include('pagination.php'); ?>
         </div>
         <div class="uk-width-1-4@m">
             <div class="uk-margin-small">

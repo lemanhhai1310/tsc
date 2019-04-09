68f504c66d8bd7eea900daff4709af2fb99c98cf
macbook <hai.lemanh.1310@gmail.com>
2019-04-09 11:38:26 +0700

    update chi tiết tin tức

---
+++ chitiettintuc.php
@@ -0,0 +1,209 @@
+<?php include('header.php'); ?>
+<div class="uk-container uk-margin-bottom">
+    <div class="box1 uk-margin">
+        <div class="uk-grid-small uk-flex-middle" uk-grid>
+            <div class="uk-width-expand">
+                <ul class="uk-breadcrumb uk-margin-small-left">
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Tin tức</a></li>
+                    <li><span>Bộ trưởng Phùng Xuân Nhạ báo cáo Chính phủ về kỳ thi THPT quốc gia</span></li>
+                </ul>
+            </div>
+            <div class="uk-width-auto">
+                <form class="uk-search uk-search-default search_h uk-visible@m">
+                    <a href="" class="uk-search-icon-flip" uk-search-icon></a>
+                    <input class="uk-search-input" type="search" placeholder="Từ khóa tìm kiếm">
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="uk-grid-small" uk-grid>
+        <div class="uk-width-expand@m">
+            <article class="box_article">
+                <h1 class="title_article">Doanh nhân Trần Anh Tuấn chia sẻ về định hướng phát triển của Dược Mỹ phẩm Korena trước thềm năm mới 2019</h1>
+                <div class="uk-flex uk-flex-middle uk-flex-between">
+                    <time class="time1">Thứ 4, 16/01/2019 | 15:25</time>
+                    <div class="fb-like fb_iframe_widget" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="action=like&amp;app_id=&amp;container_width=0&amp;href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;layout=button_count&amp;locale=en_US&amp;sdk=joey&amp;share=true&amp;show_faces=true&amp;size=small"><span style="vertical-align: bottom; width: 124px; height: 20px;"><iframe name="f1e15699c4d231c" width="1000px" height="1000px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" title="fb:like Facebook Social Plugin" src="https://www.facebook.com/v3.2/plugins/like.php?action=like&amp;app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2Fd_vbiawPdxB.js%3Fversion%3D44%23cb%3Df619b4fbad186%26domain%3Dlocalhost%26origin%3Dhttp%253A%252F%252Flocalhost%253A7942%252Ff29d7a23ee76ad%26relation%3Dparent.parent&amp;container_width=0&amp;href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;layout=button_count&amp;locale=en_US&amp;sdk=joey&amp;share=true&amp;show_faces=true&amp;size=small" style="border: none; visibility: visible; width: 124px; height: 20px;" class=""></iframe></span></div>
+                </div>
+                <ul class="uk-list xemthem">
+                    <li>03-12-2018 <a href="#">Dược Mỹ phẩm Korena 1 năm trưởng thành và phát triển vượt bậc</a></li>
+                    <li>03-12-2018 <a href="#">CEO Trần Anh Tuấn chia sẻ câu chuyện xây dựng và phát triển thương hiệu Dược Mỹ Phẩm Korena</a></li>
+                </ul>
+                <p class="desc_article">Korena là dòng dược mỹ phẩm thiên nhiên cao cấp đã không còn xa lạ gì với chị em phụ nữ Việt. Sau một thời gian ra mắt thương hiệu và phát triển nhanh chóng Korena không chỉ phát triển hệ thống đại lý trên toàn quốc mà còn xây dựng được niềm tin vững chắc với người tiêu dùng.</p>
+                <p>Chúng tôi có cuộc trò chuyện với ông Trần Anh Tuấn – Giám đốc Điều hành công ty Korena Cosmetics chia sẻ về chiến lược phát triển của Korena trong năm 2019.</p>
+                <figure class="uk-text-center">
+                    <a href="#"><img src="https://znews-photo.zadn.vn/w480/Uploaded/lerl/2019_04_09/toppp.jpg" alt=""></a>
+                    <figcaption><span class="time1">Tham dự sự kiện vinh danh đại lý xuất sắc của Korena có sự hiện diện của Hoa Hậu Biển Đào Thị Hà</span></figcaption>
+                </figure>
+                <p>Năm 2018 là năm đánh dấu sự phát triển 1 cách vượt bậc của hệ thống, từ 1 thương hiệu mới hoàn toàn chưa ai biết, hệ thống chưa có, trong vòng 8 tháng, số lượng Đại lý đã tặng vọt lên tới gần 300 đại lý, trong đó có hơn 10 NPP độc quyền tỉnh và khu vực.</p>
+                <p>Thành công của Korena là mang tới giá trị lợi ích chăm sóc làn da khỏe đẹp cho Phụ nữ Việt, mà còn tư vấn được cho hệ thống đại lý của Korena thông qua các buổi chia sẻ online về kinh doanh mỹ phẩm đầu tiên tại Việt Nam Bizschool.</p>
+                <p>PV: Trong giai đoạn đầu tiên của quá trình phát triển thương hiệu thì Korena có khó khăn, thuận lợi gì?</p>
+                <p>Giai đoạn phát triển thương hiệu Korena có khá nhiều khó khăn cũng như một số thuận lợi nhất định. Trong năm 2018 vừa qua có thể chia giai đoạn phát triển hệ thống Korena thành 2 giai đoạn.</p>
+                <p>Giai đoạn đầu: từ tháng 12/2017 – 3/2018: đây có thể coi là giai đoạn khó khăn nhất để có thể giới thiệu tới người tiêu dùng trong nước biết đến thương hiệu Korena. Bởi lẽ, thương hiệu mới rất khó có thể cạnh tranh và thuyết phục người tiêu dùng sử dụng hay đăng ký làm phân phối hệ thống, thị trường mỹ phẩm nói chung về tính cạnh tranh rất là khốc liệt. Vì vậy ban lãnh đạo cũng như đội ngũ nhân viên công ty luôn tìm mọi phương án tốt nhất, nhằm tiếp cận khách hàng giới thiệu đến được những đặc tính của sản phẩm.</p>
+            </article>
+            <div class="uk-text-right uk-margin">
+                <span class="copy_author">Theo Dân Trí</span>
+            </div>
+            <h3 class="title_news1 uk-text-uppercase">các bài viết khác</h3>
+            <ul class="uk-list list4">
+                <li><a href="#">Thông cáo báo chí Toạ đàm và Triển lãm “Phát triển nguồn nhân lực ICT trình độ cao: Gắn kết cơ sở giáo dục đại học - doanh nghiệp” <small>(28/03/2019)</small></a></li>
+                <li><a href="#">Bộ trưởng Bộ GD&ĐT Phùng Xuân Nhạ tham dự Diễn đàn giáo dục thế giới <small>(28/03/2019)</small></a></li>
+                <li><a href="#">Thông cáo báo chí về kết quả phiên tòa xét xử sơ thẩm Vụ án hành chính ông Hoàng Xuân Quế kiện Quyết định hủy bằng tiến sĩ của Bộ Giáo dục và Đào tạo <small>(28/03/2019)</small></a></li>
+                <li><a href="#">Có thêm 53 người được công nhận đạt tiêu chuẩn chức danh GS, PGS năm 2017 <small>(28/03/2019)</small></a></li>
+                <li><a href="#">Hội nghị tổng kết năm học 2016-2017, triển khai nhiệm vụ năm học 2017-2018 của ngành Giáo dục <small>(28/03/2019)</small></a></li>
+            </ul>
+        </div>
+        <div class="uk-width-1-4@m">
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner1.png" alt=""></a>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner2.png" alt=""></a>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner3.png" alt=""></a>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner4.png" alt=""></a>
+            </div>
+            <div class="uk-margin card2 uk-card uk-card-default uk-card-body uk-padding-small">
+                <div class="top_1 uk-flex uk-flex-bottom uk-flex-between uk-margin-small">
+                    <h3 class="title1 uk-text-uppercase">thư viện video</h3>
+                    <a class="uk-margin-small-right" href="#">Xem thêm <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                </div>
+                <div class="uk-margin-small">
+                    <div class="uk-cover-container">
+                        <img src="https://znews-photo.zadn.vn/w480/Uploaded/ofh_cgmzstgk/2019_04_08/f42eff6b75c0979eced1_thumb.jpg" alt="" uk-cover>
+                        <canvas width="259" height="146"></canvas>
+                        <div class="uk-position-cover uk-overlay uk-overlay-primary uk-flex uk-flex-center uk-flex-middle"></div>
+                        <div class="uk-position-center">
+                            <a href="#"><img src="imgs/btn-play1.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="uk-padding-small box3">
+                        <h3 class="title5"><a href="#">Lễ công bố và trao Quyết định điều động và bổ nhiệm Phó Giám đốc</a></h3>
+                        <time>20/10/2016 - 1.600 lượt xem</time>
+                    </div>
+                </div>
+                <div class="uk-grid-small grid-video uk-child-width-1-3" uk-grid>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/mdf_fsxrff/2019_04_08/vfs1379936251.jpg" alt="" uk-cover>
+                            <canvas width="82" height="50"></canvas>
+                            <div class="uk-position-cover uk-overlay uk-overlay-primary uk-flex uk-flex-center uk-flex-middle"></div>
+                            <div class="uk-position-center">
+                                <a href="#"><img src="imgs/btn-play2.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/kbd_ivdb/2019_04_08/20190408_195029.jpg" alt="" uk-cover>
+                            <canvas width="82" height="50"></canvas>
+                            <div class="uk-position-cover uk-overlay uk-overlay-primary uk-flex uk-flex-center uk-flex-middle"></div>
+                            <div class="uk-position-center">
+                                <a href="#"><img src="imgs/btn-play2.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/neg_glyrtla/2019_04_08/to_trinh.jpg" alt="" uk-cover>
+                            <canvas width="82" height="50"></canvas>
+                            <div class="uk-position-cover uk-overlay uk-overlay-primary uk-flex uk-flex-center uk-flex-middle"></div>
+                            <div class="uk-position-center">
+                                <a href="#"><img src="imgs/btn-play2.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-margin card2 uk-card uk-card-default uk-card-body uk-padding-small">
+                <div class="top_1 uk-flex uk-flex-bottom uk-flex-between uk-margin-small">
+                    <h3 class="title1 uk-text-uppercase">hình ảnh hoạt động</h3>
+                    <a class="uk-margin-small-right" href="#">Xem thêm <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                </div>
+                <div class="uk-grid-small grid-video uk-child-width-1-2" uk-grid>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/mdf_fsxrff/2019_04_08/vfs1379936251.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/kbd_ivdb/2019_04_08/20190408_194534_thumb.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/mdf_fsxrff/2019_04_08/84_cho_vay.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/qhj_jwrscslhfo/2019_04_08/Image2_1.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-gif.zadn.vn/Uploaded/pgi_ubnvgunau/2019_04_08/ezgifcomoptimize8.gif" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/kbd_bcvi/2019_04_07/dua_hau.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <h3 class="title6"><a href="#">Chất lượng đào tạo là thước đo trường đại học</a></h3>
+            </div>
+            <div class="uk-margin card3 uk-card uk-card-default">
+                <div class="uk-card-header uk-padding-small">
+                    <h3 class="title uk-text-uppercase">xem nhiều</h3>
+                </div>
+                <div class="uk-card-body uk-padding-small">
+                    <div class="item1">
+                        <div class="uk-grid-small" uk-grid>
+                            <div class="uk-width-auto"><i>1.</i></div>
+                            <div class="uk-width-expand">
+                                <h3 class="title4"><a href="#">Ban hành danh sách các đơn vị sự nghiệp công lập trực thuộc Bộ Giáo dục và đào tạo hôn đặc biệt khó khăn</a></h3>
+                                <time class="time1">11.07.2016 | duynguyen@gmail.com</time>
+                                <p>Chính phủ vừa ban hành Nghị định 116/2016/NĐ-CP116/2016..<a href="#">Chi tiết</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item1">
+                        <div class="uk-grid-small" uk-grid>
+                            <div class="uk-width-auto"><i>2.</i></div>
+                            <div class="uk-width-expand">
+                                <h3 class="title4"><a href="#">Ban hành danh sách các đơn vị sự nghiệp công lập trực thuộc Bộ Giáo dục và đào tạo hôn đặc biệt khó khăn</a></h3>
+                                <time class="time1">11.07.2016 | duynguyen@gmail.com</time>
+                                <p>Chính phủ vừa ban hành Nghị định 116/2016/NĐ-CP116/2016..<a href="#">Chi tiết</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item1">
+                        <div class="uk-grid-small" uk-grid>
+                            <div class="uk-width-auto"><i>3.</i></div>
+                            <div class="uk-width-expand">
+                                <h3 class="title4"><a href="#">Ban hành danh sách các đơn vị sự nghiệp công lập trực thuộc Bộ Giáo dục và đào tạo hôn đặc biệt khó khăn</a></h3>
+                                <time class="time1">11.07.2016 | duynguyen@gmail.com</time>
+                                <p>Chính phủ vừa ban hành Nghị định 116/2016/NĐ-CP116/2016..<a href="#">Chi tiết</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-card-footer uk-padding-small">
+                    <a href="#" class="uk-button uk-button-default uk-width-1-1 uk-padding-small uk-padding-remove-top uk-padding-remove-bottom btn-send1"><img class="uk-margin-small-right" src="imgs/icon2.png" alt=""> gửi câu hỏi cho tòa soạn</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('footer.php'); ?>

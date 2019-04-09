1af61f2cb6f06ea35a579964894fedd53d114aff
macbook <hai.lemanh.1310@gmail.com>
2019-04-09 11:23:51 +0700

    update danh sách tin tức

---
+++ danhsachtintuc.php
@@ -0,0 +1,225 @@
+<?php include('header.php'); ?>
+<div class="uk-container uk-margin-bottom">
+    <div class="box1 uk-margin">
+        <div class="uk-grid-small uk-flex-middle" uk-grid>
+            <div class="uk-width-expand">
+                <ul class="uk-breadcrumb uk-margin-small-left">
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><span>Tin tức</span></li>
+                </ul>
+            </div>
+            <div class="uk-width-auto">
+                <form class="uk-search uk-search-default search_h uk-visible@m">
+                    <a href="" class="uk-search-icon-flip" uk-search-icon></a>
+                    <input class="uk-search-input" type="search" placeholder="Từ khóa tìm kiếm">
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="uk-grid-small" uk-grid>
+        <div class="uk-width-expand@m">
+            <h3 class="title_c uk-text-uppercase uk-margin">tin tức</h3>
+            <div>
+                <div class="danhsach1">
+                    <div class="uk-grid-collapse" uk-grid>
+                        <div class="uk-width-auto@s">
+                            <div class="uk-cover-container">
+                                <img src="https://znews-photo.zadn.vn/w660/Uploaded/lce_cjvcc/2019_04_08/121.jpg" alt="" uk-cover>
+                                <canvas width="281" height="177"></canvas>
+                            </div>
+                        </div>
+                        <div class="uk-width-expand@s">
+                            <div class="uk-padding-small box4">
+                                <h3 class="title3 uk-margin-remove-top uk-margin-bottom" style="min-height: inherit"><a href="#">Ông Kim 'thị sát kinh tế' trước kỳ họp quốc hội Triều Tiên</a></h3>
+                                <ul class="uk-grid-small" uk-grid>
+                                    <li><i class="fa fa-clock-o" aria-hidden="true"></i> 11/7/2016</li>
+                                    <li><i class="fa fa-eye" aria-hidden="true"></i> 2.658 người xem</li>
+                                </ul>
+                                <p>Tại phiên họp Chính phủ thường kỳ tháng 7, thay mặt Bộ Giáo dục và Đào tạo (GD&ĐT), Bộ trưởng Phùng Xuân Nhạ đã báo cáo về tình hình tổ chức Kỳ thi THPT quốc gia năm 2018 và việc xử lý tiêu cực trong khâu chấm thi</p>
+                                <div class="uk-text-right">
+                                    <a href="#" class="uk-button uk-button-default btn1">chi tiết</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="danhsach1">
+                    <div class="uk-grid-collapse" uk-grid>
+                        <div class="uk-width-auto@s">
+                            <div class="uk-cover-container">
+                                <img src="https://znews-photo.zadn.vn/w660/Uploaded/lce_cjvcc/2019_04_08/121.jpg" alt="" uk-cover>
+                                <canvas width="281" height="177"></canvas>
+                            </div>
+                        </div>
+                        <div class="uk-width-expand@s">
+                            <div class="uk-padding-small box4">
+                                <h3 class="title3 uk-margin-remove-top uk-margin-bottom" style="min-height: inherit"><a href="#">Ông Kim 'thị sát kinh tế' trước kỳ họp quốc hội Triều Tiên</a></h3>
+                                <ul class="uk-grid-small" uk-grid>
+                                    <li><i class="fa fa-clock-o" aria-hidden="true"></i> 11/7/2016</li>
+                                    <li><i class="fa fa-eye" aria-hidden="true"></i> 2.658 người xem</li>
+                                </ul>
+                                <p>Tại phiên họp Chính phủ thường kỳ tháng 7, thay mặt Bộ Giáo dục và Đào tạo (GD&ĐT), Bộ trưởng Phùng Xuân Nhạ đã báo cáo về tình hình tổ chức Kỳ thi THPT quốc gia năm 2018 và việc xử lý tiêu cực trong khâu chấm thi</p>
+                                <div class="uk-text-right">
+                                    <a href="#" class="uk-button uk-button-default btn1">chi tiết</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php include('pagination.php'); ?>
+        </div>
+        <div class="uk-width-1-4@m">
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner1.png" alt=""></a>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner2.png" alt=""></a>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner3.png" alt=""></a>
+            </div>
+            <div class="uk-margin-small">
+                <a href="#"><img class="uk-width-1-1" src="imgs/banner4.png" alt=""></a>
+            </div>
+            <div class="uk-margin card2 uk-card uk-card-default uk-card-body uk-padding-small">
+                <div class="top_1 uk-flex uk-flex-bottom uk-flex-between uk-margin-small">
+                    <h3 class="title1 uk-text-uppercase">thư viện video</h3>
+                    <a class="uk-margin-small-right" href="#">Xem thêm <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                </div>
+                <div class="uk-margin-small">
+                    <div class="uk-cover-container">
+                        <img src="https://znews-photo.zadn.vn/w480/Uploaded/ofh_cgmzstgk/2019_04_08/f42eff6b75c0979eced1_thumb.jpg" alt="" uk-cover>
+                        <canvas width="259" height="146"></canvas>
+                        <div class="uk-position-cover uk-overlay uk-overlay-primary uk-flex uk-flex-center uk-flex-middle"></div>
+                        <div class="uk-position-center">
+                            <a href="#"><img src="imgs/btn-play1.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="uk-padding-small box3">
+                        <h3 class="title5"><a href="#">Lễ công bố và trao Quyết định điều động và bổ nhiệm Phó Giám đốc</a></h3>
+                        <time>20/10/2016 - 1.600 lượt xem</time>
+                    </div>
+                </div>
+                <div class="uk-grid-small grid-video uk-child-width-1-3" uk-grid>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/mdf_fsxrff/2019_04_08/vfs1379936251.jpg" alt="" uk-cover>
+                            <canvas width="82" height="50"></canvas>
+                            <div class="uk-position-cover uk-overlay uk-overlay-primary uk-flex uk-flex-center uk-flex-middle"></div>
+                            <div class="uk-position-center">
+                                <a href="#"><img src="imgs/btn-play2.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/kbd_ivdb/2019_04_08/20190408_195029.jpg" alt="" uk-cover>
+                            <canvas width="82" height="50"></canvas>
+                            <div class="uk-position-cover uk-overlay uk-overlay-primary uk-flex uk-flex-center uk-flex-middle"></div>
+                            <div class="uk-position-center">
+                                <a href="#"><img src="imgs/btn-play2.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/neg_glyrtla/2019_04_08/to_trinh.jpg" alt="" uk-cover>
+                            <canvas width="82" height="50"></canvas>
+                            <div class="uk-position-cover uk-overlay uk-overlay-primary uk-flex uk-flex-center uk-flex-middle"></div>
+                            <div class="uk-position-center">
+                                <a href="#"><img src="imgs/btn-play2.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-margin card2 uk-card uk-card-default uk-card-body uk-padding-small">
+                <div class="top_1 uk-flex uk-flex-bottom uk-flex-between uk-margin-small">
+                    <h3 class="title1 uk-text-uppercase">hình ảnh hoạt động</h3>
+                    <a class="uk-margin-small-right" href="#">Xem thêm <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                </div>
+                <div class="uk-grid-small grid-video uk-child-width-1-2" uk-grid>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/mdf_fsxrff/2019_04_08/vfs1379936251.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/kbd_ivdb/2019_04_08/20190408_194534_thumb.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/mdf_fsxrff/2019_04_08/84_cho_vay.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/qhj_jwrscslhfo/2019_04_08/Image2_1.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-gif.zadn.vn/Uploaded/pgi_ubnvgunau/2019_04_08/ezgifcomoptimize8.gif" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container">
+                            <img src="https://znews-photo.zadn.vn/w480/Uploaded/kbd_bcvi/2019_04_07/dua_hau.jpg" alt="" uk-cover>
+                            <canvas width="129" height="71"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <h3 class="title6"><a href="#">Chất lượng đào tạo là thước đo trường đại học</a></h3>
+            </div>
+            <div class="uk-margin card3 uk-card uk-card-default">
+                <div class="uk-card-header uk-padding-small">
+                    <h3 class="title uk-text-uppercase">xem nhiều</h3>
+                </div>
+                <div class="uk-card-body uk-padding-small">
+                    <div class="item1">
+                        <div class="uk-grid-small" uk-grid>
+                            <div class="uk-width-auto"><i>1.</i></div>
+                            <div class="uk-width-expand">
+                                <h3 class="title4"><a href="#">Ban hành danh sách các đơn vị sự nghiệp công lập trực thuộc Bộ Giáo dục và đào tạo hôn đặc biệt khó khăn</a></h3>
+                                <time class="time1">11.07.2016 | duynguyen@gmail.com</time>
+                                <p>Chính phủ vừa ban hành Nghị định 116/2016/NĐ-CP116/2016..<a href="#">Chi tiết</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item1">
+                        <div class="uk-grid-small" uk-grid>
+                            <div class="uk-width-auto"><i>2.</i></div>
+                            <div class="uk-width-expand">
+                                <h3 class="title4"><a href="#">Ban hành danh sách các đơn vị sự nghiệp công lập trực thuộc Bộ Giáo dục và đào tạo hôn đặc biệt khó khăn</a></h3>
+                                <time class="time1">11.07.2016 | duynguyen@gmail.com</time>
+                                <p>Chính phủ vừa ban hành Nghị định 116/2016/NĐ-CP116/2016..<a href="#">Chi tiết</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item1">
+                        <div class="uk-grid-small" uk-grid>
+                            <div class="uk-width-auto"><i>3.</i></div>
+                            <div class="uk-width-expand">
+                                <h3 class="title4"><a href="#">Ban hành danh sách các đơn vị sự nghiệp công lập trực thuộc Bộ Giáo dục và đào tạo hôn đặc biệt khó khăn</a></h3>
+                                <time class="time1">11.07.2016 | duynguyen@gmail.com</time>
+                                <p>Chính phủ vừa ban hành Nghị định 116/2016/NĐ-CP116/2016..<a href="#">Chi tiết</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-card-footer uk-padding-small">
+                    <a href="#" class="uk-button uk-button-default uk-width-1-1 uk-padding-small uk-padding-remove-top uk-padding-remove-bottom btn-send1"><img class="uk-margin-small-right" src="imgs/icon2.png" alt=""> gửi câu hỏi cho tòa soạn</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('footer.php'); ?>
---
+++ pagination.php
@@ -0,0 +1,9 @@
+<ul class="uk-pagination uk-flex-center uk-flex-middle" uk-margin>
+    <li><a href="#">Đầu</a></li>
+    <li><a href="#">Trước</a></li>
+    <li class="uk-active"><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">Sau</a></li>
+    <li><a href="#">Cuối</a></li>
+</ul>
